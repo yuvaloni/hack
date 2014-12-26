398317b58529727a41eec999645f51e972dd6474
yuvaloni <yuvon2@gmail.com>
2014-12-26 12:28:28 +0200

    regfes

---
+++ hack/WebApplication1/default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Panel ID="Panel1" runat="server">
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ hack/WebApplication1/default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Process[] pr = Process.GetProcesses();
+            foreach(Process p in pr)
+            {
+                Button btn = new Button();
+                btn.Text = p.Id + " : " + p.ProcessName + "\r\n";
+                //btn.Click += new EventHandler((object shit, EventArgs fuck) => { try { p.CloseMainWindow(); p.Kill(); } catch { } });
+                Panel1.Controls.Add(btn);
+                
+            }
+            Button Shutdwon = new Button();
+            Shutdwon.Text = ("shutdwon server");
+            //Shutdwon.Click += new EventHandler((object blah, EventArgs blahblah) => { try { Process.Start("shutdown", "/s /t 0"); } catch { } });
+            Panel1.Controls.Add(Shutdwon);
+
+        }
+    }
+}
